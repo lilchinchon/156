73fa6338ba05e0d2bf24a4abeabbb5caebc18cc0
unknown <you@example.com>
2022-08-01 22:20:27 -0500

    message

---
+++ config.js
@@ -0,0 +1,19 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const
+CONFIG = {
+    introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
+    introDesc: `I HAVE A QUESTION `,
+    btnIntro: 'OK',
+    title: 'CAN I BORROW A DOLLAR? 😙',
+    desc: 'IF U DON"T ANSWER OR U EXIT OUT IT IS MEAN U SAY YES ',
+    btnYes: 'YES IMMA LET U BORROW A THOUNDSAND DOLLAR 💖💖💖',
+    btnNo: '💔 NO WTF 💔 ',
+    question:'TELL ME WHY U LET ME BORROW UR MONEYYYY 😊😊',
+    reasonPlaceholder: 'REASON',
+    btnReply: 'SEND ! 💌💌',
+    reply: 'BC IM STUPID ASF 😊😊',
+    mess: 'IK U R STUPID SO MESSAGE ME 🥰. ',
+    messDesc: '💬💬 MESSAGE ME 💬💬',
+    btnAccept: 'Ok 💖',
+    messLink: 'https://www.instagram.com/direct/t/340282366841710300949128174471667115464?hl=en'
+}
